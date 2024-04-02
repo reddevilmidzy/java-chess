7581946a23b733b28e8d01722c74ccbf8f61502e
reddevilmidzy <midzy0228@gmail.com>
2024-04-02 13:08:33 +0900

    style: sql 쿼리 소문자로 변경

--- src/test/resources/moving.sql
+++ src/test/resources/moving.sql
@@ -2,7 +2,7 @@ use chess_test;
 
 create table moving
 (
-    movement_id INT primary key auto_increment,
+    movement_id int primary key auto_increment,
     camp        varchar(5)  not null,
     start       varchar(2) not null,
     destination varchar(2) not null
