1d6ebff5f61c9415e720175ac768326d5f2d8868
reddevilmidzy <midzy0228@gmail.com>
2024-04-04 17:47:19 +0900

    refactor: outputview private 생성자로 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,6 +7,9 @@ import view.message.ErrorCodeMessage;
 
 public class OutputView {
 
+    private OutputView() {
+    }
+
     public static void printStartMessage() {
         System.out.println("> 체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
